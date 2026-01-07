e71e886050dd3146434a4885371f77bafab32528
esnokum-dacom <esnokaskas@gmail.com>
2026-01-07 16:29:35 -0600

    fix a issue with the colorscheme

--- CsPreload.lua
+++
@@ -1,7 +0,0 @@
-local theme_file = vim.fn.stdpath("state") .. "/colorscheme.txt"
-local f = io.open(theme_file, "r")
-if f then
-    local theme = f:read("*l")
-    f:close()
-    vim.cmd.colorscheme(theme)
-end
--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 require("config.keybinds")
 require("config.vimopts")
 require("config.lazy")
-require("CsPreload")
+local theme_file = vim.fn.stdpath("state") .. "/colorscheme.txt"
+local f = io.open(theme_file, "r")
+if f then
+    local theme = f:read("*l")
+    f:close()
+    vim.cmd.colorscheme(theme)
+end
