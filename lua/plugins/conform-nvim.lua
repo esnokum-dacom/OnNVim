b499346063c7f150ca7c74c6129b1d7e741c967d
esnokum-dacom <esnokaskas@gmail.com>
2026-05-30 19:18:13 -0500

    Added & removed some stuff

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -1,28 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier" },
-				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-				typescriptreact = { "prettierd", "prettier" },
-				c = { "clang-format", prepend_args = { "--style='{BasedOnStyle: llvm, IndentWidth: 8}'" } },
-				cpp = { "clang-format" },
-				zig = { "codelldb" },
-				csproj = { "csharpier_ramboe" },
-			},
-			formatters = {
-				csharpier_ramboe = {
-					command = "csharpier",
-					args = {
-						"format",
-						"--write-stdout",
-					},
-					to_stdin = true,
-				},
-			},
-		})
-	end,
+    --   "stevearc/conform.nvim",
+    --   config = function()
+    --       require("conform").setup({
+    --           formatters_by_ft = {
+    --               lua = { "stylua" },
+    --               javascript = { "prettierd", "prettier", stop_after_first = true },
+    --               typescript = { "prettierd", "prettier" },
+    --               javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+    --               typescriptreact = { "prettierd", "prettier" },
+    --               c = { "clang-format", prepend_args = { "--style='{BasedOnStyle: llvm, IndentWidth: 8}'" } },
+    --               cpp = { "clang-format" },
+    --               zig = { "codelldb" },
+    --               csproj = { "csharpier_ramboe" },
+    --           },
+    --           formatters = {
+    --               csharpier_ramboe = {
+    --                   command = "csharpier",
+    --                   args = {
+    --                       "format",
+    --                       "--write-stdout",
+    --                   },
+    --                   to_stdin = true,
+    --               },
+    --           },
+    --       })
+    --   end,
 }
--- lua/plugins/render-md.lua
+++ lua/plugins/render-md.lua
@@ -1,7 +1,1 @@
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-}
+return {}
